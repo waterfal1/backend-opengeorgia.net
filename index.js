488e4be7d28780843c242a2370d8351818296af5
f1ne2 <aliaksei.tamilouski@softensity.com>
2024-04-12 16:05:45 +0200

    reset cors url headers

--- index.js
+++ index.js
@@ -25,10 +25,7 @@ router.get("/myendpoint", async (_req, res, _next) => {
     timestamp: Date.now(),
   };
   try {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://backend-opengeorgia-net.vercel.app"
-    );
+    res.header("Access-Control-Allow-Origin", "https://www.opengeorgia.net/");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type,token");
     res.send(healthcheck);
@@ -60,10 +57,7 @@ const app = express();
 app.use(cors());
 
 const allowCrossDomain = function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://backend-opengeorgia-net.vercel.app"
-  );
+  res.header("Access-Control-Allow-Origin", "https://www.opengeorgia.net/");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,token");
   next();

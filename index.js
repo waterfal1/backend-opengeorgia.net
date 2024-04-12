323eeaa5edb6d1288c3d62c5151d0166a0bee041
f1ne2 <aliaksei.tamilouski@softensity.com>
2024-04-12 15:52:36 +0200

    reset cors url headers

--- index.js
+++ index.js
@@ -25,11 +25,10 @@ router.get("/myendpoint", async (_req, res, _next) => {
     timestamp: Date.now(),
   };
   try {
-    res.header("Access-Control-Allow-Origin", [
-      "https://opengeorgia.net",
-      "frontend-opengeorgia-git-master-waterfal1s-projects.vercel.app",
-      "https://www.opengeorgia.net",
-    ]);
+    res.header(
+      "Access-Control-Allow-Origin",
+      "frontend-opengeorgia-git-master-waterfal1s-projects.vercel.app"
+    );
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type,token");
     res.send(healthcheck);
@@ -61,10 +60,10 @@ const app = express();
 app.use(cors());
 
 const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", [
-    "https://opengeorgia.net",
-    "frontend-opengeorgia-git-master-waterfal1s-projects.vercel.app",
-  ]);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "frontend-opengeorgia-git-master-waterfal1s-projects.vercel.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,token");
   next();

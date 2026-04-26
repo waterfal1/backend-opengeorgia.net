7f7c75142af45b4cfcaffdc99ac9b0caaff3e157
waterfal1 <alextomilovski@gmail.com>
2026-04-27 00:15:17 +0200

    change domain

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ router.get("/myendpoint", async (_req, res, _next) => {
   try {
     res.header(
       "Access-Control-Allow-Origin",
-      "https://backend-opengeorgia-net.vercel.app/myendpoint",
+      "https://backend-opengeorgia-net.vercel.app",
     );
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type,token");
@@ -60,7 +60,10 @@ const app = express();
 app.use(cors());
 
 const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.opengeorgia.net");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://backend-opengeorgia-net.vercel.app",
+  );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,token");
   next();
